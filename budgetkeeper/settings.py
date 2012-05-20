d31626ac4a2de4b164f55548391ba6dca45ea81e
C Nelson <cnelsonsic@gmail.com>
2012-05-20 01:19:42 -0500

    Add an example settings file.

---
+++ budgetkeeper/settings.py
@@ -0,0 +1,17 @@
+
+# SMTP Settings:
+SMTP_SERVER = 'smtp.gmail.com'
+USE_AUTHENTICATION = True
+USE_STARTTLS = True # Or SSL
+TLS_PORT = 587
+SSL_PORT = 465
+
+# IMAP Settings:
+IMAP_SERVER = 'imap.gmail.com'
+USE_SSL = True
+IMAP_PORT = 993
+
+# Shared settings:
+ACCOUNT_NAME = "example@gmail.com"
+EMAIL_ADDRESS = ACCOUNT_NAME
+PASSWORD = "examplepassword"
